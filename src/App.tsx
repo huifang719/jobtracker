c6790d766850524b9ceff0a80fd008d1c7552bba
livia <livia.gu@notitia.consulting>
2024-03-16 10:15:48 +1030

    change endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,134 +1,146 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import PageHeader from './components/inc/PageHeader'
-import './App.css'
-import { FieldValues } from 'react-hook-form';
-import { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { listingJob, reset } from './features/jobSlice';
-import JobBoard from './components/JobBoard';
-import Home from './components/Home';
-import SignUp from './components/SignUp';
-import LogIn from './components/LogIn';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import { removeUser, setUser } from './features/userSlice';
-import { setSavedJob } from './features/savedJobList';
+import PageHeader from "./components/inc/PageHeader";
+import "./App.css";
+import { FieldValues } from "react-hook-form";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { listingJob, reset } from "./features/jobSlice";
+import JobBoard from "./components/JobBoard";
+import Home from "./components/Home";
+import SignUp from "./components/SignUp";
+import LogIn from "./components/LogIn";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { removeUser, setUser } from "./features/userSlice";
+import { setSavedJob } from "./features/savedJobList";
 
-const App:React.FC = ():JSX.Element => {
-  const loggedInEmail = useSelector((state: any) => state.user.value.email)
-  const [errorMessage, setErrorMessage] = useState<string|null>(null)
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+const App: React.FC = (): JSX.Element => {
+  const loggedInEmail = useSelector((state: any) => state.user.value.email);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const logIn = async(data:FieldValues) => {
-    const response = await fetch('/api/sessions', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
-    }) 
-      .then(res => res.json())
-      
-    if (response.error) return setErrorMessage(response.error) 
-    await dispatch(setUser({email: response}))           
-    await setErrorMessage(null)
-    await navigate('/')         
-  }
-  
-  const getSavedJobList = async()  => {
-    if (!loggedInEmail) return dispatch(reset())
-    
-    const response = await fetch(`/api/save/${loggedInEmail}`)
-      .then(res => res.json())
-    
-    if (response.error) return console.log(response.error)
-    
-    return await dispatch(setSavedJob(response))
-  }   
-  
+  const logIn = async (data: FieldValues) => {
+    const response = await fetch(
+      "https://jobtracker-typescript.onrender.com/api/sessions",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      }
+    ).then((res) => res.json());
+
+    if (response.error) return setErrorMessage(response.error);
+    await dispatch(setUser({ email: response }));
+    await setErrorMessage(null);
+    await navigate("/");
+  };
+
+  const getSavedJobList = async () => {
+    if (!loggedInEmail) return dispatch(reset());
+
+    const response = await fetch(
+      `https://jobtracker-typescript.onrender.com/api/save/${loggedInEmail}`
+    ).then((res) => res.json());
+
+    if (response.error) return console.log(response.error);
+
+    return await dispatch(setSavedJob(response));
+  };
+
   useEffect(() => {
     getSavedJobList();
-    checkSession()
+    checkSession();
   }, [loggedInEmail]);
 
-  const checkSession = async() => {
-    const response = await fetch('/api/sessions')
-    .then(res => res.json())
+  const checkSession = async () => {
+    const response = await fetch(
+      "https://jobtracker-typescript.onrender.com/api/sessions"
+    ).then((res) => res.json());
 
-    if (response.error) return console.log(response.error)
-    
-    return await dispatch(setUser({email: response}))
-  }
+    if (response.error) return console.log(response.error);
 
-  const signUp = async(data: FieldValues)=> {
-    const response= await fetch('/api/users', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data)
-    })
-    .then(res => res.json())
+    return await dispatch(setUser({ email: response }));
+  };
 
-    if (response.error) return setErrorMessage(response.error)
-    await setErrorMessage(null)
-    await navigate('/login')
-  }
+  const signUp = async (data: FieldValues) => {
+    const response = await fetch(
+      "https://jobtracker-typescript.onrender.com/api/users",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      }
+    ).then((res) => res.json());
 
-  const logOut = async() => {
-    const response = await fetch('/api/sessions', {
-      method: 'DELETE'
-    })
-    .then(res => res.json())
+    if (response.error) return setErrorMessage(response.error);
+    await setErrorMessage(null);
+    await navigate("/login");
+  };
 
-    if (!response.error) return dispatch(removeUser({email: null}))
-  }
-  const handleSearch = async(data: FieldValues) => {
+  const logOut = async () => {
+    const response = await fetch(
+      "https://jobtracker-typescript.onrender.com/api/sessions",
+      {
+        method: "DELETE",
+      }
+    ).then((res) => res.json());
+
+    if (!response.error) return dispatch(removeUser({ email: null }));
+  };
+  const handleSearch = async (data: FieldValues) => {
     if (!loggedInEmail) {
-      navigate('/login')  
-      return setErrorMessage('Please login first')
+      navigate("/login");
+      return setErrorMessage("Please login first");
     }
 
-    dispatch(reset())
+    dispatch(reset());
 
-    const api_key = await fetch('/api/search')
-      .then(res => res.json())
-  
-    const { title, location} = data
-    const response= await fetch(`https://api.adzuna.com/v1/api/jobs/au/search/1?app_id=6fe66bca&app_key=${api_key}&title_only=${title}&where=${location}`)
-      .then(res => res.json())
-    if (response.error) return console.log('miao')
+    const api_key = await fetch(
+      "https://jobtracker-typescript.onrender.com/api/search"
+    ).then((res) => res.json());
+
+    const { title, location } = data;
+    const response = await fetch(
+      `https://api.adzuna.com/v1/api/jobs/au/search/1?app_id=6fe66bca&app_key=${api_key}&title_only=${title}&where=${location}`
+    ).then((res) => res.json());
+    if (response.error) return console.log("miao");
 
     await response.results.forEach((job: any) => {
-      const { title, location, description, redirect_url, company
-      } = job 
-      dispatch(listingJob({title: title, location: location['display_name'], description: description, url: redirect_url, company:company['display_name']}))
-    }) 
-    await navigate('/jobboard')
-  }
-  
+      const { title, location, description, redirect_url, company } = job;
+      dispatch(
+        listingJob({
+          title: title,
+          location: location["display_name"],
+          description: description,
+          url: redirect_url,
+          company: company["display_name"],
+        })
+      );
+    });
+    await navigate("/jobboard");
+  };
+
   return (
     <div className="App">
       <header className="App-header me-0">
-        <PageHeader 
-          logOut = { logOut }
-          handleSearch={handleSearch}
-        />
+        <PageHeader logOut={logOut} handleSearch={handleSearch} />
       </header>
       <main>
         <Routes>
-          <Route path='/' element={<Home />} 
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/signup"
+            element={<SignUp signUp={signUp} errorMessage={errorMessage} />}
           />
-          <Route path='/signup' element={<SignUp
-            signUp={signUp} 
-            errorMessage = { errorMessage }/>} 
+          <Route
+            path="/login"
+            element={<LogIn logIn={logIn} errorMessage={errorMessage} />}
           />
-          <Route path='/login' element={<LogIn 
-            logIn = {logIn}
-            errorMessage = { errorMessage }/>} 
-          />
-          <Route path='/jobboard' element={<JobBoard  />}  
-          />
+          <Route path="/jobboard" element={<JobBoard />} />
         </Routes>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/JobBoard.tsx
+++ src/components/JobBoard.tsx
@@ -1,91 +1,122 @@
-import { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux';
-import { Container, Button, Card } from 'react-bootstrap';
-import { IconContext } from "react-icons"
-import { AiFillHeart, AiOutlineHeart } from "react-icons/ai"
-import Job from "../Jobs"
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Container, Button, Card } from "react-bootstrap";
+import { IconContext } from "react-icons";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import Job from "../Jobs";
 
-const JobBoard: React.FC  = () => {
-  const jobs = useSelector((state:any) => state.job.value)
-  console.log(jobs)
-  const [saveStatus, setSaveStatus] = useState<boolean[]>([])
-  const loggedInEmail = useSelector((state: any) => state.user.value.email)
+const JobBoard: React.FC = () => {
+  const jobs = useSelector((state: any) => state.job.value);
+  console.log(jobs);
+  const [saveStatus, setSaveStatus] = useState<boolean[]>([]);
+  const loggedInEmail = useSelector((state: any) => state.user.value.email);
 
   // I would like the site to fetch the status whether the job has been saved or not from the backend, then saved into the state, so that jobs wont be saved twice or more times
-  const init = () =>{
-    jobs.length>0&&jobs.forEach((job:Job) => {
-    var  description= job.description
-    const response = fetch(`/api/save/${loggedInEmail}/${description}`) 
-      .then(res => res.json())
-      .then(res => {
-        if (res.error) return setSaveStatus(saveStatus=>[...saveStatus, false])       
-        return setSaveStatus(saveStatus=>[...saveStatus, true])
-        })
-    });
-  } 
-  
+  const init = () => {
+    jobs.length > 0 &&
+      jobs.forEach((job: Job) => {
+        var description = job.description;
+        const response = fetch(
+          `https://jobtracker-typescript.onrender.com/api/save/${loggedInEmail}/${description}`
+        )
+          .then((res) => res.json())
+          .then((res) => {
+            if (res.error)
+              return setSaveStatus((saveStatus) => [...saveStatus, false]);
+            return setSaveStatus((saveStatus) => [...saveStatus, true]);
+          });
+      });
+  };
+
   //only fetch and save the status everytime job list changes, so if user search for new jobs, we get new set of status according to user saved history
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(init, [jobs])
+  useEffect(init, [jobs]);
 
-  const toggle = (index:number) => {
+  const toggle = (index: number) => {
     //checking the state, if it's saved, toggle will delete the job then change the state
-    if (saveStatus[index]) return deleteJob(index)
-     
+    if (saveStatus[index]) return deleteJob(index);
+
     //if not, it will save the job on server and change the state
-    return saveJob(index)
-  }
+    return saveJob(index);
+  };
 
-  const saveJob = async(index:number) => {
-    const jobtoBeSaved = jobs[index]
-    
-    const response=await fetch('/api/save', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify([jobtoBeSaved, loggedInEmail])
-      })
-      .then(res => res.json())
-    await console.log('miao')
-    if (response.error) return console.log(response.error)
+  const saveJob = async (index: number) => {
+    const jobtoBeSaved = jobs[index];
 
-    return await setSaveStatus(saveStatus.map((status,i) => i === index? true : status))
-  }
-  
+    const response = await fetch(
+      "https://jobtracker-typescript.onrender.com/api/save",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify([jobtoBeSaved, loggedInEmail]),
+      }
+    ).then((res) => res.json());
+    await console.log("miao");
+    if (response.error) return console.log(response.error);
 
-  const deleteJob = async(index: number) => {
-    const description = jobs[index].description
-    const response = await fetch(`/api/save/${description}`, {
-      method: 'DELETE'
-    })
-      .then(res => res.json())
-    
-    return await setSaveStatus(saveStatus.map((status,i) => i === index? false: status))
-  }
-  
+    return await setSaveStatus(
+      saveStatus.map((status, i) => (i === index ? true : status))
+    );
+  };
+
+  const deleteJob = async (index: number) => {
+    const description = jobs[index].description;
+    const response = await fetch(
+      `https://jobtracker-typescript.onrender.com/api/save/${description}`,
+      {
+        method: "DELETE",
+      }
+    ).then((res) => res.json());
+
+    return await setSaveStatus(
+      saveStatus.map((status, i) => (i === index ? false : status))
+    );
+  };
+
   return (
     <>
       <Container className="d-block g-2">
-        <IconContext.Provider value={{color:"rgb(110,223,94)", size:"2rem"}}>
-          {jobs.length>0&&jobs.map((job:Job, index: number) =>       
-            <Card key={index} className='mb-2'>
-              <Card.Header className='d-flex justify-content-between'>
-                <Card.Text>Location: {job.location}</Card.Text>
-                {job.company&&<Card.Text>Company: {job.company}</Card.Text>}
-              </Card.Header>
-              <Card.Body>
-                <Card.Title>{job.title}</Card.Title>
-                <Card.Text>
-                  {job.description}
-                </Card.Text>
-                <Button style={{backgroundColor:"rgb(110,223,94)", border:"none"}} href={job.url}>Read more</Button>
-                <Button className='col' variant="outline-*" style={{border:"none"}} onClick={ ()=> toggle(index) }>{saveStatus[index]=== true? <AiFillHeart /> : <AiOutlineHeart />}</Button>
-              </Card.Body>
-            </Card>
-          )}
-        </IconContext.Provider>  
-      </Container>     
+        <IconContext.Provider
+          value={{ color: "rgb(110,223,94)", size: "2rem" }}
+        >
+          {jobs.length > 0 &&
+            jobs.map((job: Job, index: number) => (
+              <Card key={index} className="mb-2">
+                <Card.Header className="d-flex justify-content-between">
+                  <Card.Text>Location: {job.location}</Card.Text>
+                  {job.company && <Card.Text>Company: {job.company}</Card.Text>}
+                </Card.Header>
+                <Card.Body>
+                  <Card.Title>{job.title}</Card.Title>
+                  <Card.Text>{job.description}</Card.Text>
+                  <Button
+                    style={{
+                      backgroundColor: "rgb(110,223,94)",
+                      border: "none",
+                    }}
+                    href={job.url}
+                  >
+                    Read more
+                  </Button>
+                  <Button
+                    className="col"
+                    variant="outline-*"
+                    style={{ border: "none" }}
+                    onClick={() => toggle(index)}
+                  >
+                    {saveStatus[index] === true ? (
+                      <AiFillHeart />
+                    ) : (
+                      <AiOutlineHeart />
+                    )}
+                  </Button>
+                </Card.Body>
+              </Card>
+            ))}
+        </IconContext.Provider>
+      </Container>
     </>
-  )
-}
+  );
+};
 
 export default JobBoard;
